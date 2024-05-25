3f268fcde1909efd2ee2ae0e09770519ce142b55
linpeiqi <1536715858@qq.com>
2024-05-26 01:46:55 +0800

    refactor: 微调

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,10 @@
 // List of categories for blog posts
-export const CATEGORIES = ['Basic', 'Build', 'Project', 'Category 4', 'Category 5'] as const
+export const CATEGORIES = [
+	'Basic',
+	'Build',
+	'Node',
+	'Project',
+	'Algorithm',
+	'Network',
+	'Other'
+] as const
